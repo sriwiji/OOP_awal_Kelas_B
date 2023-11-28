2f11f620fc6fb322e3903f2890c185c618ac68e1
sriwiji <150015752+sriwiji@users.noreply.github.com>
2023-11-28 15:07:17 +0700

    sriwiji_ 2255201058.php

--- 2255201058.php
+++ 2255201058.php
@@ -3,60 +3,52 @@
 <body>
 
 <?php
-class mahasiswa{
-  // Properties
-  private $name;
-  private $nim;
-  private $mata_kuliah;
-  private $dosen_killer;
+class Mahasiswa {
 
-  // Methods
-  function set_name($name) {
-    $this->name = $name;
+  private $Nama;
+  private $NIM;
+  private $MataKuliah;
+  private $dosenkiller;
+  private $citacita;
+
+
+  public function __construct($Nama, $NIM, $MataKuliah, $dosenkiller, $citacita){
+    $this->Nama = $Nama;
+    $this->NIM = $NIM;
+    $this->MataKuliah = $MataKuliah;
+    $this->dosenkiller = $dosenkiller;
+    $this->citacita = $citacita;
   }
-  function get_name() {
-    return $this->name;
+
+
+  function get_Nama() {
+    return $this->Nama;
   }
-  function set_nim($nim) {
-    $this->nim = $nim;
+  function get_NIM(){
+    return $this->NIM;
   }
-  function get_nim() {
-    return $this->nim;
+  function get_MataKuliah(){
+    return $this->MataKuliah;
   }
-   function set_mata_kuliah($mata_kuliah) {
-    $this->mata_kuliah= $mata_kuliah;
+  function get_dosenkiller(){
+    return $this->dosenkiller;
   }
-  function get_mata_kuliah() {
-    return $this->mata_kuliah;
-  }
-  function set_dosen_killer($dosen_killer) {
-    $this->dosen_killer= $dosen_killer;
-  }
-  function get_dosen_killer() {
-    return $this->dosen_killer;
+    function get_citacita(){
+    return $this->citacita;
   }
 }
 
-$sri_wiji_astuti = new mahasiswa();
-$sri_wiji_astuti->set_name('Sri_Wiji_Astuti');
-$sri_wiji_astuti->set_nim('2255201058');
-$sri_wiji_astuti->set_mata_kuliah('pbo');
-$sri_wiji_astuti->set_dosen_killer('arif mudi priyanto.s.t.m.kom');
-
-echo $sri_wiji_astuti ->get_name();
+$sriwiji = new Mahasiswa('sriwiji', '2255201058', 'PBO', 'bapak arip', 'pengusaha');
+echo $sriwiji->get_Nama();
 echo "<br>";
-echo $sri_wiji_astuti ->get_nim();
+echo $sriwiji->get_NIM();
 echo "<br>";
-echo $sri_wiji_astuti ->get_mata_kuliah();
+echo $sriwiji->get_MataKuliah();
 echo "<br>";
-echo $sri_wiji_astuti ->get_dosen_killer();
-
-
-
-
-
-
+echo $sriwiji->get_dosenkiller();
+echo "<br>";
+echo $sriwiji->get_citacita();
 ?>
- 
+
 </body>
 </html>
